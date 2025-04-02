6e2ba246c3456af4d38b6535f790ac60edd54c62
laurarusanescu <117192875+laurarusanescu@users.noreply.github.com>
2025-04-02 12:54:40 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,61 @@
+<br/>
+
+<h3 align="center">
+  <img src="https://github.com/simonwep/ocular/assets/30767528/2cbd76cd-adfb-4183-a69a-15784c4dccb6" alt="Logo" height="150">
+</h3>
+
+<br/>
+
+<h3 align="center">
+  ✨ <a href="https://budget.reinisch.io">Ocular</a> - Helps you see your budgets more clearly ✨
+</h3>
+
+<p align="center">
+  <a href="https://github.com/simonwep/ocular/actions?query=workflow%3ACI"><img
+     alt="CI Status"
+     src="https://github.com/simonwep/ocular/workflows/CI/badge.svg"/></a>
+  <a href="https://github.com/simonwep/ocular/releases"><img
+     alt="Version"
+     src="https://img.shields.io/github/v/release/simonwep/ocular?label=Version&color=%233e5df3"/></a>
+  <a href="https://github.com/simonwep/ocular/releases"><img
+     alt="Latest Release"
+     src="https://img.shields.io/github/release-date/simonwep/ocular?label=Release&color=%233e5df3"/></a>
+</p>
+
+> [!TIP]
+> Check out the [demo](https://ocular.reinisch.io#demo) to get a live preview with demo data!
+
+## Features
+
+- 🦾 Self-hosted. [Deploy](https://github.com/simonwep/ocular-docker) it via docker-compose in seconds.
+- 🕶️ Beautiful light and dark theme.
+- 💻 Installable as a PWA and ready for mobile.
+- 🔼 Import your data from Google sheet's annual planner.
+- 🛠️ Export your data as a single `.json`-file.
+- ✨ Dashboard with sankey diagram and a detailed view.
+- 🎇 Track budgets across multiple years.
+- 🙈 Privacy mode for when you're in a public place.
+- ⚡️ Simple, fast, and straight-forward UI.
+- 🫂 User management and authentication.
+- 🌐 Multi-language and currency support.
+- ...and much more!
+
+## Summary
+
+Ocular is a small budgeting app as an alternative to Google sheet's annual budget planner.
+Its goal is **not** to track individual expenses, work with multiple currencies at a time or anything related (if you're looking for something like this, check out [firefly-iii](https://www.firefly-iii.org/)).
+
+This app comes with its own backend (by using [genesis](https://github.com/simonwep/genesis)), so the only thing you need to do is to host it somewhere.
+Your data stays on your server and is not shared with anyone else.
+
+## Getting started
+
+### Production
+
+Best deploy this app via [docker-compose](https://docs.docker.com/compose/).
+See [ocular-docker](https://github.com/simonwep/ocular-docker) for deployment.
+Don't worry, it'll only take a few minutes to set everything up.
+
+### Local development
+
+If you want to work on this, make sure to out the [contributing guidelines](.github/CONTRIBUTING.md) :)
---
+++ vite.config.ts
@@ -0,0 +1,118 @@
+import { minifyHtmlPlugin } from './plugins/vite-plugin-minify-html/minifyHtmlPlugin';
+import { minifyJsonPlugin } from './plugins/vite-plugin-minify-json/minifyJsonPlugin';
+import vue from '@vitejs/plugin-vue';
+import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
+import { VitePWA } from 'vite-plugin-pwa';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  envPrefix: ['OCULAR'],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        rewrite: (path) => path.slice(4) // cut off `/api`
+      }
+    }
+  },
+  preview: {
+    port: 3000
+  },
+  define: {
+    'import.meta.env.OCULAR_BUILD_TIMESTAMP': Date.now()
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@styles': '/src/styles', // Keep if needed
+      '@directives': '/src/directives',
+      '@components': '/src/app/components',
+      '@app': '/src/app',
+      '@i18n': '/src/i18n',
+      '@storage': '/src/storage',
+      '@utils': '/src/utils',
+      '@store': '/src/store'
+    }
+  },
+  build: {
+    target: 'ES2022'
+  },
+  plugins: [
+    tsconfigPaths({ loose: true }),
+    optimizeCssModules(),
+    minifyJsonPlugin(),
+    minifyHtmlPlugin(),
+    vue({
+      script: {
+        defineModel: true
+      }
+    }),
+    VitePWA({
+      manifest: {
+        name: 'Ocular',
+        short_name: 'Ocular',
+        start_url: '/',
+        display: 'standalone',
+        orientation: 'any',
+        background_color: '#fff',
+        theme_color: '#6bb1ff',
+        description: 'Budgeting app',
+        icons: [
+          {
+            src: '/images/icon-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/images/icon-maskable-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/images/icon-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/images/icon-maskable-1024x1024.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/images/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/images/icon-1024x1024.png',
+            sizes: '1024x1024',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,woff2,json}']
+      }
+    })
+  ],
+  test: {
+    include: ['./src/**/*.spec.ts']
+  }
+});
